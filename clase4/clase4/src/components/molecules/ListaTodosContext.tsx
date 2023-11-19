61038186ca9be703eeac206ab956e75555bf705d
Adrian Blanco <ablancoc@protonmail.com>
2023-11-19 17:22:46 -0500

    Problemas menores

--- clase4/clase4/src/components/molecules/ListaTodosContext.tsx
+++ clase4/clase4/src/components/molecules/ListaTodosContext.tsx
@@ -1,40 +1,31 @@
 import React from "react";
 import { useEjemploContext } from "../../contexts/Ejemplo";
-import { ITodo } from "../../models/ITodo";
-import React from 'react'
-import { useEjemploContext } from '../../contexts/Ejemplo'
-import { Todo } from './Todo';
+import { Todo } from "./Todo";
 
 export const ListaTodosContext = () => {
     const { todos, loading, agregarTodo } = useEjemploContext();
-    console.log(todos);
 
-
-    if (loading) return <p>Cargando...</p>
+    if (loading) return <p>Cargando...</p>;
 
     const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-
             agregarTodo(e.currentTarget.value);
-
             e.currentTarget.value = "";
         }
-    }
+    };
 
     return (
         <>
-            <input type="text" onKeyDown={handleEnter} />
+            <input
+                type="text"
+                onKeyDown={handleEnter}
+                placeholder="Ingrese un nuevo todo"
+            />
             <ul>
                 {todos.map((todo) => {
-                    return <Todo key={todo.id} todo={todo}></Todo>
-
-
-
+                    return <Todo key={todo.id} todo={todo} />;
                 })}
-
-
-
-            </ul >
+            </ul>
         </>
-    )
+    );
 };
--- clase4/clase4/src/contexts/Ejemplo.tsx
+++ clase4/clase4/src/contexts/Ejemplo.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React from "react";
 import { ITodo } from "../models/ITodo";
 import { IUser } from "../models/IUser";
